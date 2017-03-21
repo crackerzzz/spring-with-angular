6d770f7c1fffacd415d028831ee358b0dde90862
crackerzzz <shreejwal.shrestha@gmail.com>
2017-03-21 16:49:51 +1100

    finished till wro change in phase 2

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,7 +1,27 @@
-angular.module('hello', [])
-.controller('home', function($http) {
-	var self  = this;
-	$http.get('resource/').then(function(response) {
-		self.greeting = response.data;
-	});
-})
+angular
+		.module('hello', [ 'ngRoute' ])
+		.config(
+				function($routeProvider, $httpProvider) {
+
+					$routeProvider
+					.when('/', {
+						templateUrl : 'home.html',
+						controller : 'home',
+						controllerAs : 'controller'
+					})
+					.when('/login', {
+						templateUrl : 'login.html',
+						controller : 'navigation',
+						controllerAs : 'controller'
+					}).otherwise('/');
+
+					$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+				})
+		.controller('home', function($http) {
+			var self = this;
+			$http.get('resource/').then(function(response) {
+				self.greeting = response.data;
+			});
+		})
+		.controller('navigation', function() {});
